1fc6a4abda9ca9a56eb8419eda938269b030cc20
jackwise254 <jackwise179@gmail.com>
2023-09-13 08:42:08 -0700

    Betty coding style updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0.
- *
- * Return: void.
+ * times_table - print the 9 times table, starting with 0
  */
 void times_table(void)
 {
@@ -14,28 +12,24 @@ void times_table(void)
         for (j = 0; j <= 9; j++)
         {
             product = i * j;
-
             if (j != 0)
             {
-                if (product >= 10)
+                _putchar(',');
+                _putchar(' ');
+                if (product <= 9)
+                {
                     _putchar(' ');
-                else
-                    _putchar(' ');
-                    _putchar(' ');
+                }
             }
-
-            if (product >= 10)
+            if (product > 9)
             {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
+                _putchar(product / 10 + '0');
+                _putchar(product % 10 + '0');
             }
             else
             {
                 _putchar(product + '0');
             }
-
-            if (j != 9)
-                _putchar(',');
         }
         _putchar('\n');
     }
