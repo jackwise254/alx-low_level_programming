42555ffe5258940246d6a56a6c3128f6c96a45e5
jackwise254 <jackwise179@gmail.com>
2023-09-28 13:17:52 -0700

    we try again

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,8 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')  /* Base case */
-    {
-        return 0;
-    }
+	if (*s == '\0')  /* Base case */
+		return (0);
 
-    return 1 + _strlen_recursion(s + 1);  /*Recursive call */
+	return (1 + _strlen_recursion(s + 1));  /* Recursive call */
 }
