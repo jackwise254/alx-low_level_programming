46e2e6983773473fa8832e9244a337c5049d1f01
jackwise254 <jackwise179@gmail.com>
2023-09-28 13:06:08 -0700

    new changes

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')  // Base case
+    if (*s == '\0')  /* Base case */
     {
         return;
     }
     
-    _print_rev_recursion(s + 1);  // Recursive call for next character
-    _putchar(*s);  // Print current character after the recursive call
+    _print_rev_recursion(s + 1);  /* Recursive call for next character */
+    _putchar(*s);  /* Print current character after the recursive call*/
 }
