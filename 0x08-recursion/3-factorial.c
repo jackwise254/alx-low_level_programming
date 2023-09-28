f71e29a2d363f1887f5c31f7df365c77adf7175a
jackwise254 <jackwise179@gmail.com>
2023-09-28 13:20:20 -0700

    factorials

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,10 @@
  */
 int factorial(int n)
 {
-    if (n < 0)  // Error case
-    {
-        return (-1);
-    }
-    else if (n == 0)  // Base case
-    {
-        return (1);
-    }
-    return n * factorial(n - 1);  // Recursive case
+	if (n < 0)  /* Error case */
+		return (-1);
+	else if (n == 0)  /* Base case */
+		return (1);
+
+	return (n * factorial(n - 1));  /* Recursive case */
 }
