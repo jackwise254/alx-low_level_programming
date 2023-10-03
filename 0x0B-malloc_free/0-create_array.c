196d9164bab2b0b4ca65c58eaee85f469863365b
jack <jack@kali.example.com>
2023-10-03 16:56:55 +0300

    corrected betty coding style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,19 +9,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *arr;
+	unsigned int i;
+	char *arr;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    arr = (char *)malloc(size);
-    
-    if (arr == NULL)
-        return (NULL);
+	arr = (char *)malloc(size);
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = c;
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 
-    return (arr);
+	return (arr);
 }
