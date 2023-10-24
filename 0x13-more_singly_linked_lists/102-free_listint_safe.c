d2f78c2c7c3ec84d3e5cfdf9d87b89b4c7702e10
jackwise254 <jackwise179@gmail.com>
2023-10-24 00:40:58 -0700

    correction

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,23 +1,34 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * is_node_visited - checks if a node has been visited before.
- * @addr: address of node to check
- * @visited_nodes: array of addresses to check against
- * @index: number of nodes visited so far
+ * find_loop - find the node where the loop starts
+ * using Floyd's Cycle-Finding Algorithm.
+ * @head: pointer to the head of the list
  *
- * Return: 1 if node has been visited, 0 otherwise
+ * Return: address of the node where loop starts, or NULL if there is no loop
  */
-int is_node_visited(const listint_t *addr, const listint_t *visited_nodes[], size_t index)
+listint_t *find_loop(listint_t *head)
 {
-    size_t i;
-    for (i = 0; i < index; i++)
+    listint_t *tortoise = head;
+    listint_t *hare = head;
+
+    while (tortoise && hare && hare->next)
     {
-        if (visited_nodes[i] == addr)
-            return (1);
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        if (tortoise == hare)
+        {
+            tortoise = head;
+            while (tortoise != hare)
+            {
+                tortoise = tortoise->next;
+                hare = hare->next;
+            }
+            return hare;
+        }
     }
-    return (0);
+
+    return NULL;
 }
 
 /**
@@ -29,20 +40,32 @@ int is_node_visited(const listint_t *addr, const listint_t *visited_nodes[], siz
 size_t free_listint_safe(listint_t **h)
 {
     size_t count = 0;
-    listint_t *tmp;
-    const listint_t *visited_nodes[1024];
+    listint_t *tmp, *loop_start;
 
-    while (*h && !is_node_visited(*h, visited_nodes, count))
+    if (!h || !(*h))
+        return 0;
+
+    loop_start = find_loop(*h);
+
+    while (*h)
     {
-        visited_nodes[count] = *h;
+        if (*h == loop_start && count > 0)
+            break;
+
         tmp = *h;
         *h = (*h)->next;
         free(tmp);
         count++;
+
+        if (*h && !(*h)->next && *h == loop_start) 
+        {
+            free(*h);
+            *h = NULL;
+            count++;
+        }
     }
 
-    *h = NULL;  // Set the head to NULL after freeing
-
-    return (count);
+    *h = NULL;
+    return count;
 }
 
