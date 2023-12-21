78e84571bc9ca57304eb71849546e03935e5992f
jackwise254 <jackwise179@gmail.com>
2023-12-21 12:12:17 -0800

    testing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,34 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: A pointer to the newly created hash table, or NULL if failure.
+ * Return: A pointer to the newly created hash table, or NULL if it fails.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
+    hash_table_t *new_table;
     unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
+    if (size == 0)
         return (NULL);
 
-    /* Allocate memory for the array of pointers to nodes */
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
+    new_table = malloc(sizeof(hash_table_t));
+    if (new_table == NULL)
+        return (NULL);
+
+    new_table->size = size;
+    new_table->array = malloc(sizeof(hash_node_t *) * size);
+    if (new_table->array == NULL)
     {
-        free(hash_table); /* Free hash_table if array allocation fails */
+        free(new_table);
         return (NULL);
     }
 
-    /* Initialize each array element to NULL */
     for (i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
+        new_table->array[i] = NULL;
 
-    hash_table->size = size;
-
-    return (hash_table);
+    return (new_table);
 }
 
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,29 +1,34 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-/* Node structure for hash table */
-typedef struct hash_node
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key (string)
+ * @value: The value corresponding to the key
+ * @next: A pointer to the next node in the collision resolution
+ */
+typedef struct hash_node_s
 {
     char *key;
     char *value;
-    struct hash_node *next;
+    struct hash_node_s *next;
 } hash_node_t;
 
-/* Hash table structure */
-typedef struct hash_table
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the array
+ * @array: An array of size @size containing pointers to nodes
+ */
+typedef struct hash_table_s
 {
     unsigned long int size;
     hash_node_t **array;
 } hash_table_t;
 
-/* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-void hash_table_delete(hash_table_t *ht);
-void hash_table_print(const hash_table_t *ht);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif /* HASH_TABLES_H */
 
